46be2b75ce6e6c40248578ffc1027c4a0f0c9919
Sam Wellander <samwellander@gmail.com>
2018-09-27 12:10:20 -0400

    some more styles

--- src/components/StudentDetail.js
+++ src/components/StudentDetail.js
@@ -2,34 +2,83 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import { _deleteStudent } from '../store/student';
-import { Paper, Grid, Avatar } from '@material-ui/core';
+import { Button, Typography, Paper, Grid, Avatar } from '@material-ui/core';
 
 const StudentDetail = ({ student, remove }) => {
   if (!student) return <Redirect to="/schools" />
   const avatarStyles = {
     width: 200,
-    height: 200
+    height: 200,
+    margin: 20
+  }
+  const styles = {
+    marginTop: '10vh'
   }
   return (
-    <div>
-
+    <div style={styles}>
       <Grid justify="center" container>
         <Grid item xs={4}>
           <Paper>
-            <Avatar
-              style={avatarStyles}
-              src={student.imageUrl}
-              alt={student.name}
-            />
-            <h2>{student.firstName} {student.lastName}</h2>
-            {/* <img src={student.imageUrl} /> */}
-            <p><em>{student.gpa}</em></p>
-            <button><Link to={`/students/${student.id}/update`}>Update</Link></button>
-            <button onClick={() => remove(student.id)}>Delete</button>
+            <Grid container justify="center">
+              <Grid item>
+                <Avatar
+                  style={avatarStyles}
+                  src={student.imageUrl}
+                  alt={student.name}
+                />
+              </Grid>
+            </Grid>
+            <Grid spacing={8} container>
+
+              {/* first row */}
+              <Grid item xs={5}>
+                <Typography variant="subheading" align="right">
+                  Name:
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography>
+                  {student.firstName} {student.lastName}
+                </Typography>
+              </Grid>
+
+              {/* second row */}
+              <Grid item xs={5}>
+                <Typography variant="subheading" align="right">
+                  GPA:
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography>
+                  {student.gpa}
+                </Typography>
+              </Grid>
+              {/* third row */}
+              <Grid item xs={5}>
+                <Typography variant="subheading" align="right">
+                  School:
+                </Typography>
+              </Grid>
+              <Grid item xs={7}>
+                <Typography to={`/schools/${student.schoolId}`} component={Link}>
+                  {student.school.name}
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid justify="center" container spacing={24}>
+              <Grid align="center" item xs={3}>
+                <Button to={`/students/${student.id}/update`} component={Link} variant="contained" color="primary">Update</Button>
+              </Grid>
+              <Grid align="center" item xs={3}>
+                <Button variant="contained" color="secondary" onClick={() => remove(student.id)}>Delete</Button>
+              </Grid>
+            </Grid>
+
           </Paper>
         </Grid>
       </Grid>
-    </div>
+    </div >
   )
 }
 
