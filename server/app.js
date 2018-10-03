98f7cae5429ebd25a8310ef2d9e698a2dcbda543
Sam Wellander <samwellander@gmail.com>
2018-10-03 19:42:46 -0400

    remove morgan

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 
 app.use(express.json());
-// app.use(morgan());
 app.use('/api', require('./api'));
 app.use(express.static('public'));
 
