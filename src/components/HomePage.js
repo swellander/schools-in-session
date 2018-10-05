db5c7c75b59827b4be8ec0487b8e0b420a571d8b
Sam Wellander <samwellander@gmail.com>
2018-10-05 09:46:18 -0400

    add delete modal/dialog

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -28,7 +28,6 @@ class HomePage extends Component {
             )}
           </Grid>
         </Grid>
-
       </div>
     )
   }
--- src/components/SchoolDetail.js
+++ src/components/SchoolDetail.js
@@ -3,10 +3,28 @@ import SchoolTabs from './SchoolTabs';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 import { _deleteSchool } from '../store/school';
-import { Typography, Paper, Grid, Button, Divider } from '@material-ui/core';
+import { Typography, Paper, Grid, Button, Divider, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
 
 class SchoolDetail extends React.Component {
-
+  constructor() {
+    super();
+    this.state = {
+      open: false
+    }
+    this.handleClose = this.handleClose.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+  handleClose() {
+    this.setState({ open: false })
+  }
+  handleOpen() {
+    this.setState({ open: true })
+  }
+  handleRemove() {
+    this.props.remove(this.props.school.id);
+    this.handleClose();
+  }
   render() {
     const { user, students, school, remove } = this.props;
     const styles = {
@@ -36,7 +54,7 @@ class SchoolDetail extends React.Component {
                     <Grid item xs={3}>
                       <div style={btnStyles}>
                         <Button to={`/schools/${school.id}/update`} component={Link} variant="contained" color="primary">Update</Button>
-                        <Button variant="contained" color="secondary" onClick={() => remove(school.id)}>Delete</Button>
+                        <Button variant="contained" color="secondary" onClick={this.handleOpen}>Delete</Button>
                       </div>
                     </Grid>
                   )}
@@ -61,6 +79,33 @@ class SchoolDetail extends React.Component {
             </Paper>
           </Grid>
         </Grid>
+
+        {/* Modal.....Maybe make a separate component? This one is getting pretty loooooong */}
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{`Are you sure?`}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {`Deleting ${school.name} would mean depriving ${school.students.length} ${school.students.length === 1 ? 'student' : 'students'} of a quality education. ${school.students.length > 2 ? (
+                `Think of ${school.students[0].firstName} and ${school.students[1].firstName}. Take a good long look at ${school.students[2].firstName} ${school.students[2].lastName}'s goofy cartoon profile picture and then make your decision.`
+              ) : (
+                  `Well, actually, there really aren't that many students that would be affected by this destructive decision. Delete away.`
+                )}`}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleRemove} color="secondary">
+              Delete
+            </Button>
+            <Button onClick={this.handleClose} color="primary" autoFocus>
+              Save the Children
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div >
     )
   }
